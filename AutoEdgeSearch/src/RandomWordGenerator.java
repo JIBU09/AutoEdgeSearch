7d1ee756ad3e0c2cea53f0599ba5a5ee3f837171
JIBU09 <jimkazandjian@gmx.de>
2024-04-19 14:24:42 +0200

    opening tabs

--- AutoEdgeSearch/src/RandomWordGenerator.java
+++ AutoEdgeSearch/src/RandomWordGenerator.java
@@ -1,7 +1,11 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+
+import java.awt.*;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -9,9 +13,16 @@ import java.util.Random;
 public class RandomWordGenerator {
     public static void main(String[] args) {
         List<String> englishWords = fetchEnglishWords();
-        int numberOfWords = 1;
+        int numberOfWords = 20;
         String[] randomWords = generateRandomWords(englishWords, numberOfWords);
         for (String word : randomWords) {
+            String URL = "https://www.bing.com/search?q=" + spaceReplacer(word);
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(URL));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
             System.out.println(word);
         }
     }
@@ -47,5 +58,14 @@ public class RandomWordGenerator {
         Random random = new Random();
         return (char) ('a' + random.nextInt(23));
     }
+
+    public static String spaceReplacer(String string) {
+        if (string.contains(" ")) {
+            return string.replace(" ", "+");
+        } else {
+            return string;
+        }
+    }
+
 }
 
